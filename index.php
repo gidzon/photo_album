8bbcb75fceefcca2489095fca33aa910249781dc
Nicolay Vorobev <sumakaki37rus@gmail.com>
2020-03-10 09:51:08 +0300

    добавил блоки

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php include_once 'includes/header.html'; ?>    
+    <div class="main-content">
+        <div class="content">
+           <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+           <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+           <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+           <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+           <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+        <div class="content">
+            <a href="#"><img src="images/photo1.jpg"></a>
+            <a href="#"><p>nickname</p></a>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
